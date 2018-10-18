abba4b72d63d2f3d3b2c9fdb2832ba6eb7e1d562
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-18 14:12:42 +0200

    home

--- src/App.js
+++ src/App.js
@@ -3,33 +3,50 @@ import './App.css';
 
 const Body = ({ activeTab }) => {
   if (activeTab === "about") {
-    return <h1>about</h1> //
+    return <AboutTab /> //
   } else {
-    return <h1>home</h1>
+    return <HomeTab />
   }
+}
+
+function Tab({ title, tab, changeTab }) {
+  return <button onClick={() => changeTab(tab)}>
+    {title}
+  </button>
+}
+
+function HomeTab() {
+  return <h1>inhoud van de homepage</h1>
+}
+
+function AboutTab() {
+  return <h1>inhoud van de aboutpage</h1>
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { activeTab: "home" }
+    this.changeTab = this.changeTab.bind(this)
   }
-  render() {
 
+  changeTab(tab) {
+    this.setState({
+      activeTab: tab,
+    })
+  }
+
+  render() {
     return (
       <div>
-        <button onClick={() => {
-          this.setState({
-            activeTab: "home"
-          })
-        }}>Home</button>
+        <Tab title="Front" tab="home" changeTab={this.changeTab} />
         <button onClick={() => {
           this.setState({
             activeTab: "about"
           })
         }}>About</button>
         <Body activeTab={this.state.activeTab} />
-        {/* {this.state.activeTab} */}
+
       </div>
     );
   }
