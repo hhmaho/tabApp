e36f68da81f9cc7889a1a81d60ef95645c430245
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-18 12:46:53 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+const Body = ({ activeTab }) => (
+  <h1>test</h1>
+)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: "home" }
+  }
   render() {
+
     return (
-      <div className="App">
+      <div>
+        <button onClick={() => {
+          this.setState({
+            activeTab: "home"
+          })
+        }}>Home</button>
+        <button onClick={() => {
+          this.setState({
+            activeTab: "about"
+          })
+        }}>About</button>
+        <Body />
+        {this.state.activeTab}
       </div>
     );
   }
